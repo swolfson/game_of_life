eaca08082b25a3d26b701af849b847a81eac2924
swolfson <swolfson2003@gmail.com>
2013-08-21 19:07:59 -0400

    updated a bit

--- analyze_life.py
+++ analyze_life.py
@@ -3,24 +3,35 @@ from matplotlib import pyplot
 
 from life import LifeGame
 
-results = {'death': 0, 'loop': 0, 'stable': 0}
 i = 0
-numgames = 100
-size = 10
-numcells = 200
+numgames = 10
+size = 15
+numcells = 15
+fullResults = []
 
-while(i < numgames):
+for j in range(10,150,5):
 
-	game = LifeGame(size,numcells)
-	game.play(False)
-	if game.endcode == 1:
-		results['death'] += 1
-	elif game.endcode == 2:
-		results['stable'] += 1
-	elif game.endcode == 3:
-		results['loop'] += 1
-	i += 1
+	results = {'initial cells': 0, 'death': 0, 'loop': 0, 'stable': 0}
+	initcells = 0
+	while(i < numgames):
+		
+		game = LifeGame(size,j)
+		initcells +=game.game[-1].total_live_cells()
+		game.play(False)
+		if game.endcode == 1:
+			results['death'] += 1
+		elif game.endcode == 2:
+			results['stable'] += 1
+		elif game.endcode == 3:
+			results['loop'] += 1
+		i += 1
+	results['initial cells'] = initcells/numgames
+	fullResults.append(results)
+	i = 0
 
-print "on a size %d board with %d initial cells:" %(size,numcells)
-print results
+print "size of board: %d square" %size
+for item in fullResults:
+	print "average initial cells: %d " %item['initial cells'],
+	print "deaths: %d, loops: %d, stable: %d" %(item['death'],item['loop'],
+			item['stable'])
 
